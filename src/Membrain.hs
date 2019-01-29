1baf263ebe03eed8b8d50a7a8ee29a7a78ea07ff
Dmitry Kovanikov <kovanikov@gmail.com>
2019-01-29 08:25:07 +0800

    [#5] Implement pretty Show instance for Memory

--- src/Membrain.hs
+++ src/Membrain.hs
@@ -1,9 +1,11 @@
 -- | Type-safe memory units.
 
 module Membrain
-       ( module Membrain.Memory
+       ( module Membrain.Constructors
+       , module Membrain.Memory
        , module Membrain.Units
        ) where
 
+import Membrain.Constructors
 import Membrain.Memory
 import Membrain.Units
