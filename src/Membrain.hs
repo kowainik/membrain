974632fdd970ea477718e0dc2ffbc1dcb08bafee
vrom911 <vrom911@gmail.com>
2019-05-20 16:43:05 +0800

    [#7] Impl some base functions
    Resolves #7

--- src/Membrain.hs
+++ src/Membrain.hs
@@ -1,11 +1,13 @@
 -- | Type-safe memory units.
 
 module Membrain
-       ( module Membrain.Constructors
+       ( module Membrain.Base
+       , module Membrain.Constructors
        , module Membrain.Memory
        , module Membrain.Units
        ) where
 
+import Membrain.Base
 import Membrain.Constructors
 import Membrain.Memory
 import Membrain.Units
