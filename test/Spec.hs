5bfa622dac132b2c84a53248ab3518ab6dc2098f
Dmitrii Kovanikov <dmitrii@holmusk.com>
2019-05-29 16:56:34 +0800

    [#8] Implement basic unit tests (#28)
    Resolves #8

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,14 +2,22 @@ module Main (main) where
 
 import Hedgehog (Group (..), checkParallel)
 import System.Exit (exitFailure, exitSuccess)
+import System.IO (hSetEncoding, stderr, stdout, utf8)
+import Test.Hspec (hspec)
 
 import Test.Memory.Laws (monoidIdentityLaw, semigroupLaw)
 import Test.Memory.TypeLevel (runTypeLevelTests)
+import Test.Memory.ValueLevel (unitTests)
 
 
 main :: IO ()
 main = do
+    -- fix terminal encoding
+    hSetEncoding stdout utf8
+    hSetEncoding stderr utf8
+
     runTypeLevelTests
+    hspec unitTests
     checkParallel hedgehogTests >>= \p -> if p then exitSuccess else exitFailure
 
 hedgehogTests :: Group
