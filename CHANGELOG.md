3143281e743e3204cf6253b2d0f16d4b4680d4b4
Veronika Romashkina <vrom911@gmail.com>
2020-05-11 10:25:44 +0100

    [#48] Support 8.10 (#49)
    * [#48] Support 8.10
    
    Resolves #48
    
    * Update haddock for main reexport module
    
    * Test show details on CI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@
 `membrain` uses [PVP Versioning][1].
 The changelog is available [on GitHub][2].
 
+## 0.0.0.2 — 11 May, 2020
+
+* [#48](https://github.com/kowainik/membrain/issues/48):
+  Support GHC-8.10. Upgrade GHC-8.8 to 8.8.3.
+
 ## 0.0.0.1 — 09 Feb, 2020
 
 * Upgrade to GHC-8.8.2.
--- src/Membrain.hs
+++ src/Membrain.hs
@@ -3,14 +3,7 @@ Copyright: (c) 2018-2020 Kowainik
 SPDX-License-Identifier: MPL-2.0
 Maintainer: Kowainik <xrom.xkov@gmail.com>
 
-Type-safe memory units. This package has the following structure:
-
-* __"Membrain.Memory":__ main 'Memory' data type with many utility functions.
-* __"Membrain.Units":__ type-level unit multipliers.
-* __"Membrain.Constructors":__ smart constructors for creating values of type
-  'Memory'.
-* __"Membrain.Base":__ type-safe versions of functions from @base@ that work
-  with memory-related values.
+Type-safe memory units.
 
 @membrain@ aims for qualified imports. Import as follows:
 
@@ -20,13 +13,34 @@ __import__ __qualified__ Membrain __as__ Mem
 -}
 
 module Membrain
-       ( module Membrain.Base
-       , module Membrain.Constructors
-       , module Membrain.Memory
+       ( -- $mem
+         module Membrain.Memory
+         -- $units
        , module Membrain.Units
+         -- $constructors
+       , module Membrain.Constructors
+         -- $base
+       , module Membrain.Base
        ) where
 
 import Membrain.Base
 import Membrain.Constructors
 import Membrain.Memory
 import Membrain.Units
+
+{- $mem
+Main 'Memory' data type with many utility functions.
+-}
+
+{- $units
+Type-level unit multipliers.
+-}
+
+{- $constructors
+Smart constructors for creating values of type 'Memory'.
+-}
+
+{- $base
+Type-safe versions of functions from @base@ that work with memory-related
+values.
+-}
